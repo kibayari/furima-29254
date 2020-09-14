2c54b0a33d8a9fddae9cece4032f55c6249bf1d5
kibayari <kibayari09@gmail.com>
2020-09-14 19:07:31 +0900

    ルートindex.html.erb実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # get 'homes/index'
   devise_for :users
   #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#index'
+  resources :user
 end
