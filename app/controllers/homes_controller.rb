7cbf32c8ea5d5a508a91483a7a84ac1582926c04
kibayari <kibayari09@gmail.com>
2020-09-15 18:26:36 +0900

    new&createアクション追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,27 @@
 class HomesController < ApplicationController
+
   def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    #バリデーションで問題があれば、保存はされず戻る
+    if @user.valid?
+      @user.save
+      redirect_to root_path
+    else
+      #保存されなければ、newに戻る
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+     params.require(:user).permit(:nickname, :email, :password)
   end
 end
