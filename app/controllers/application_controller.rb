3cf8e5d18ef934d7d6fc56a6122ea6cc05f0a246
kibayari <kibayari09@gmail.com>
2020-09-15 18:25:32 +0900

    applicationcontroller設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # データーベースへのカラム内容の保存許可
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :password, :last_name, :first_name_kana, :last_name_kana, :email, :birth_date])
+  end
 end
