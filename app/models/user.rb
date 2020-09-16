49dd1938ea8ed543b2aa0d88608437f1ac8dd929
kibayari <kibayari09@gmail.com>
2020-09-16 16:26:21 +0900

    新規登録画面文字数制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,18 @@ class User < ApplicationRecord
 
   # ニックネーム漢字およびカタカナでしか入力させない（正規表現）
   with_options presence: true do
+    validates :nickname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   end
+
+  validates :nickname, presence: true, length: { maximum: 20 }
+  validates :first_name, presence: true, length: { maximum: 20 }
+  validates :last_neme, presence: true, length: { maximum: 20 }
+  validates :first_name, presence: true, length: { maximum: 20 }
+  validates :last_name, presence: true, length: { maximum: 20 }
+  validates :password, presence: true, length: { maximum: 20 }
 end
