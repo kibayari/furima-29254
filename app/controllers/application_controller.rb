e00bf388b1103f0cf1749f42d2bdf46557bf6eb3
kibayari <kibayari09@gmail.com>
2020-09-16 14:21:34 +0900

    ログイン有無での表示分岐

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
   #ログインしていないユーザーをログイン画面に
-  before_action :authenticate_user!
-
+  # before_action :authenticate_user!
+  
   # データーベースへのカラム内容の保存許可
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :password, :last_name, :first_name_kana, :last_name_kana, :email, :birth_date])
   end
