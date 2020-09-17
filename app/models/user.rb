9391b2b4281baccc6299483810dce1bae223f9ee
kibayari <kibayari09@gmail.com>
2020-09-17 12:00:00 +0900

    新規登録生年月日のエラー文表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,22 +5,37 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # 空で投稿出来ないように追記
-  # validates :nickname, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date, :email, presence: true
+  validates :birth_date, presence: true
 
   # ニックネーム漢字およびカタカナでしか入力させない（正規表現）
-  with_options presence: true do
-    validates :nickname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  end
+  # with_options presence: true do
+    # validates :nickname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    # validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    # validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    # validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    # validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    # validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  # end
 
-  validates :nickname, presence: true, length: { maximum: 20 }
-  validates :first_name, presence: true, length: { maximum: 20 }
-  validates :last_name, presence: true, length: { maximum: 20 }
-  validates :first_name, presence: true, length: { maximum: 20 }
-  validates :last_name, presence: true, length: { maximum: 20 }
-  validates :password, presence: true, length: { maximum: 20 }
+  # validates :nickname, presence: true, length: { maximum: 20 }
+  # validates :first_name, presence: true, length: { maximum: 20 }
+  # validates :last_name, presence: true, length: { maximum: 20 }
+  # validates :first_name, presence: true, length: { maximum: 20 }
+  # validates :last_name, presence: true, length: { maximum: 20 }
+  # validates :password, presence: true, length: { maximum: 20 }
+
+  with_options presence: true do
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i },length: { maximum: 20 }
+    
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } , length: { maximum: 20 } do
+      validates :nickname
+      validates :first_name
+      validates :last_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } , length: { maximum: 20 } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+  end
 end
