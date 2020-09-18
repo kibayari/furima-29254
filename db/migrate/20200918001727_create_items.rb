262456066db934f15679d16e0be3f5065bebacbd
kibayari <kibayari09@gmail.com>
2020-09-18 09:34:14 +0900

    itemsテーブルの作成

--- db/migrate/20200918001727_create_items.rb
+++ db/migrate/20200918001727_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string  :name,             null: false
+      t.integer :price,            null: false
+      t.text    :item_info,        null: false
+      t.integer :category,         null: false
+      t.integer :status_id,        null: false
+      t.integer :delivery_fee_id,  null: false
+      t.integer :shipping_area_id, null: false
+      t.integer :delivery_days_id, null: false
+      t.integer :user_id, null: false, foreign_key:true 
       t.timestamps
     end
   end
