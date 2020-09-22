7465cd81309eec217cafe84dd3bfed74b71fcf4d
kibayari <kibayari09@gmail.com>
2020-09-22 15:22:59 +0900

    商品登録空の場合のエラーメッセージ表記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,8 @@ class ItemsController < ApplicationController
 
  def create
     @item = Item.new(items_params)
-    if @item.save
+    if @item.valid?
+       @item.save
        redirect_to root_path
     else
       render :new
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,2 @@
 class Article < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre, :comdition, :delivery, :shipping, :deliberyday
-  
-
-  #空の投稿を保存できないようにする
-  validates :title, :text, :genre, :comdition_id, :delivery_id, :shipping, :deliberyday, presence: true
-
-  #ジャンルの選択が「--」の時は保存できないようにする
-  validates :genre_id, :comdition_id, :delivery_id, :shipping, :deliberyday, numericality: { other_than: 1 } 
 end
