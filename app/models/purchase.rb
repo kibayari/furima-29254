b3f1afb1cf01bdbe076485ede82e404ae76ca7bd
kibayari <kibayari09@gmail.com>
2020-09-26 22:00:20 +0900

    購入モデルに中途だった設定内容を停止

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,5 @@
-class Purchase < ApplicationRecord
- has_one :address, dependent: :destroy
- belongs_to :user
- belongs_to :items
-end
+# class Purchase < ApplicationRecord
+# #  has_one :address, dependent: :destroy
+# #  belongs_to :user
+# #  belongs_to :item
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_many :purchases, dependent: :destroy
+  # has_many :purchases, dependent: :destroy
 
   with_options presence: true do
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
