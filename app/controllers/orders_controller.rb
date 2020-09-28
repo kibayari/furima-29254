4237b9ce01c4758bbd09b47c4d1c07048562bc75
kibayari <kibayari09@gmail.com>
2020-09-28 20:13:04 +0900

    カード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,40 @@
 class OrdersController < ApplicationController
   def index
-    @order = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
   end
 
+  # def new
+  #   @order = Orders.new
+  # end
+
   def create
-    @order = Orders.create(orders_params)
-    redirect_to root_path
+    @order = Order.new(price: order_params[:price])
+    # @order = Orders.create(orders_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
-  def orders_params
-    params.require(:item).permit(:image, :name, :price, :item_info, :genre_id, :comdition_id, :delivery_id, :shipping_id, :deliberyday_id, ).merge(user_id: current_user.id)
+  # def orders_params
+  #   params.require(:item).permit(:image, :name, :price, :item_info, :genre_id, :comdition_id, :delivery_id, :shipping_id, :deliberyday_id, ).merge(user_id: current_user.id)
+  # end
+
+  def order_params
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_6528c01e805d7c02039be1a6"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
   end
 
 end
