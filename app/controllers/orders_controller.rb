f206f799a6af0ac2a3d6875c9e8ff43e78a9382e
kibayari <kibayari09@gmail.com>
2020-10-01 20:05:51 +0900

    条件分岐自分の商品購入させない

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,9 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @user_orders = UserOrders.new
+    if current_user.id == @item.user_id 
+       redirect_to root_path
+    end 
   end
 
   def edit
